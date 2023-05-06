df5d3a14a1934f611f052990ecc419cf8a5098e9
Jonathke <jonathan.eriksen97@gmail.com>
2023-05-06 17:25:28 +0200

    links

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 document.write(`
-    <a href="index.html" class = "button" style = "background-color: #e6fff2;">Initial</a><br>
-    <a href="documents.html" class = "button" style = "background-color: #ccffe6;">Objects</a><br>
-    <a href="other.html" class = "button" style = "background-color: #b3ffd9;">Adjoints</a><br>
+    <a href="./" class = "button" style = "background-color: #e6fff2;">Initial</a><br>
+    <a href="./documents" class = "button" style = "background-color: #ccffe6;">Objects</a><br>
+    <a href="./other" class = "button" style = "background-color: #b3ffd9;">Adjoints</a><br>
 `);
