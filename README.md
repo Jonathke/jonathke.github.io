df71d46861286278862a9442c09cb401f4a65e34
Jonathke <jonathan.eriksen97@gmail.com>
2023-05-06 16:38:01 +0200

    First commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# jonathke.github.io
---
+++ navbar.js
@@ -0,0 +1,5 @@
+document.write(`
+    <a href="index.html" class = "button" style = "background-color: #e6fff2;">Initial</a><br>
+    <a href="documents.html" class = "button" style = "background-color: #ccffe6;">Objects</a><br>
+    <a href="other.html" class = "button" style = "background-color: #b3ffd9;">Adjoints</a><br>
+`);
