bc2f204b690ee93d28f15a0235c7e57481145891
Jonathke <jonathan.eriksen97@gmail.com>
2024-03-28 10:18:12 +0100

    blog thingy added

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 document.write(`
     <a href="./" class = "button" style = "background-color: #e6fff2;">Initial</a><br>
     <a href="./documents" class = "button" style = "background-color: #ccffe6;">Objects</a><br>
-    <a href="./other" class = "button" style = "background-color: #b3ffd9;">Adjoints</a><br>
-    <a href="./blog" class = "button" style = "background-color: #77feba;">Morphisms</a><br>
+    <a href="./other" class = "button" style = "background-color: #b3ffd9;">Morphisms</a><br>
+    <a href="./blog" class = "button" style = "background-color: #9affcc;">Adjoints</a><br>
 `);
